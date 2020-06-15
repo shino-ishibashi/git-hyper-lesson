a2b050dcae9c0e8d98d75020dcc17a0045d0e58b
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-06-15 17:08:33 +0900

    車が焦げるメソッド作ったよ

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -3,26 +3,31 @@ package com.example;
 public class Car {
 	private Integer speed;
 	private String color;
-	
+
 	public void run() {
 		this.speed += 500;
 	}
-	
+
 	public void brake() {
 		this.speed = 0;
 	}
-	
-	
-	
+
+	public void burn() {
+		this.color = "ブラック";
+	}
+
 	public Integer getSpeed() {
 		return speed;
 	}
+
 	public void setSpeed(Integer speed) {
 		this.speed = speed;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
